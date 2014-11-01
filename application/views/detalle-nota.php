da9cc8e6823a9c9f774e4118427bc76fc54f2d53
nerdytrust <contacto@nerdytrust.com>
2014-11-01 14:43:39 -0600

    Type commit message here…

---
+++ application/views/detalle-nota.php
@@ -0,0 +1,113 @@
+<div class="main">
+
+	<div class="units-row first-main"  >
+	    <div class="unit-100">
+	    
+	        <div class="hiddenmobile" style="display: inline-block;float: left;padding-top: 5px">
+	                    México D.F. a <?=strftime('%A %d de %B de %Y', time())?> 
+	        </div>
+	      
+	        <div style="display: inline-block;padding-top: 1px;">    
+	                <input type="text" name="go" class="input-on-black"  placeholder="Búsqueda" />
+	                <span class="btn-append">
+	                    <button class="btn btn-white btn-outline" style="margin-right:5px;">Go</button>
+	                </span>
+	        </div>
+	    </div>
+	</div>
+
+	<div class="units-row">
+
+		<div class="unit-100">
+			<div>
+	            <img src="http://lorempixel.com/640/270/people/" alt="" width="100%">
+	            <div class="imgholder-container unit-65">
+	                <div class="imgholder">
+	                    Comisiones especiales de San Lázaro: muchos gastos y poco trabajo.
+	                </div>
+	            </div>
+        	</div>		
+		</div>
+		
+	</div>
+
+	<div class="units-row">
+		<div class="unit-100">
+						<img src="images/icons/user.png" width="60" alt="">
+						<div style="display: inline-block;padding-top:0.5em;">
+							<br/>Redacción  10 Agosto 2014<br/>
+							<a href="">Seguir en Twitter &nbsp;<img src="images/icons/twitter-256.png" width="20" alt=""></a>
+						</div>
+
+			<div class="item-body">
+            <br>
+            
+            	<p>El diputado <strong>Ricardo Mejía Berdeja</strong> abandonó el salón entre una oleada de preguntas. Lo esperaban casi todos los reporteros que cubren la <strong>Cámara de Diputados</strong>. La razón: momentos antes había nacido otra <strong>comisión especial</strong>, no cualquiera,  la que revisaría un tema peliagudo en las esferas del poder en México: los casinos.</p>
+				<p><span style="line-height: 1.3em;">Meses atrás, el 16 de enero de 2013, Mejía Berdeja y el coordinador de la bancada del partido Movimiento Ciudadano, Ricardo Monreal Ávila, propusieron a la<strong> Comisión Permanente del Congreso</strong> un punto de acuerdo para revisar los permisos otorgados a los casinos de todo el país. Ese mismo día se aprobó y el 13 de febrero ocurriría lo mismo pero en el Pleno de San Lázaro. Ni el PRI ni el PAN pusieron resistencia.</span></p>
+				<p><span style="line-height: 1.3em;">Pocas veces la instalación de una comisión especial causa tanto revuelo como el provocado la tarde del jueves 4 de abril de 2013, a pesar de que la puesta en funcionamiento implica más gasto público. Casi siempre pasan desapercibidas pero esta tocaba incluso a altos funcionarios de la administración federal anterior. Los reporteros y fotógrafos trataron de abrirse paso al salón antes de que la sesión iniciara, pero los elementos de seguridad impidieron el paso.</span><span style="line-height: 1.3em;"> </span></p>
+				<p>Los preparativos de la instalación comenzaron alrededor de las 13:30 horas. Diez minutos después, el lugar estaba atestado. Mejía Berdeja ocupó el lugar principal de la mesa que no lo marca el mismo mueble –que es circular- sino la pared principal, una estructura de madera de la que cuelga el escudo de México y la leyenda “Junta de Coordinación Política”.  A diferencia de otras ocasiones, ahora n<strong>o hubo comida ni servicio de edecanes ni café ni galleta</strong>s, simplemente agua.</p>
+				<p><span style="line-height: 1.3em;">Mejía Berdeja, de traje oscuro impecable y corbata naranja, declaró instalada la <strong>Comisión Especial de Juegos y Sorteos</strong> a las 13:50 horas, tras una breve ronda de intervenciones de todos los integrantes del nuevo órgano legislativo. Él sería su primer presidente. El ritual terminó a las 14:30 horas y con eso bastó para recibir presupuesto propio y comenzar operaciones. El dinero era la certeza; los resultados, la apuesta.</span></p>
+				<p><span style="line-height: 1.3em;">La misma ceremonia ha ocurrido 35 veces durante la LXII Legislatura en la Cámara de Diputados. <strong>Son 35 las comisiones especiales.</strong> Son 35 las que reciben dinero y recursos materiales para alcanzar sus objetivos, pocas veces claros y menos aún alcanzados. Los beneficios incluyen asesores, auxiliares administrativos, telefonía celular, automóvil y dinero para boletos de avión.</span></p>
+				<p><span style="line-height: 1.3em;">El <strong>artículo 207 del Reglamento de la Cámara de Diputados</strong> establece que el Pleno autoriza la creación de comisiones especiales a petición de la <strong>Junta de Coordinación Política.</strong> El objetivo es analizar temas específicos, importantes y/o coyunturales. Nunca podrán representar más de la mitad de las ordinarias. Una vez que el asunto se declara concluido, deberán disolverse.</span></p>
+				<p><span style="line-height: 1.3em;">Las comisiones especiales –dicta el artículo 208– deben presentar un plan y calendario de trabajo, así como informes semestrales en los que transparenten <strong>“el destino final de los recursos económicos y materiales utilizados durante cada año legislativo”</strong>.  La de Juegos y Sorteos es de las pocas que cumple con estas obligaciones.</span></p>
+				<p>De acuerdo con la página de Internet de la Cámara de Diputados, la Comisión Especial de Juegos y Sorteos lleva 11 reuniones desde que surgió, hace 16 meses. Sólo ha presentado un informe que abarca del 4 de abril de 2013 al 27 de marzo de 2014. Le falta uno. El reglamento ordena uno cada semestre, no cada seis meses acumulados en los periodos ordinarios de sesiones.</p>
+				<p><span style="line-height: 1.3em;">Treinta y dos de 35 comisiones especiales de diputados de la actual Legislatura se instalaron entre noviembre de 2012 y abril de 2013. La más nueva se creó el 26 de agosto de 2014. Las primeras ofrecen un lapso suficiente para medir la productividad. Según el <a href="http://sitl.diputados.gob.mx/LXII_leg/listado_de_comisioneslxii.php?tct=2" target="_blank"><strong>portal de Internet de la Cámara:</strong></a></span></p>
+				<p><span style="line-height: 1.3em;">-       Hasta el 18 de abril de 2014, <strong>solo 11 comisiones especiales presentaron al menos</strong> un informe de actividades. En otras palabras, sólo una de cada 3 cumple con el reglamento.</span></p>
+				<p><span style="line-height: 1.3em;">-       Hasta el 24 de mayo de 2014,<strong> las  comisiones suman 139 reuniones.</strong> El promedio es entonces de 4 reuniones: una cada 3 meses y medio.</span></p>
+				<p><span style="line-height: 1.3em;">El reglamento no obliga a las comisiones especiales a presentar iniciativas de ley, pero tampoco lo prohíbe. Pese a esto,<strong> rara vez el trabajo de uno de estos órganos colegiados se traduce en modificaciones o propuestas de ley.</strong> En la presente Legislatura, sólo la comisión de Programas Sociales lo ha hecho y logró que la aprobara el Pleno.</span></p>
+				<p><span style="line-height: 1.3em;">¿Cuánto cuesta a los ciudadanos que cumplen con el pago de impuestos la labor de las comisiones especiales? No es posible saberlo con exactitud debido a la opacidad en San Lázaro. El 31 octubre de 2013, pedimos a la <strong>Unidad de Transparencia de la Cámara de Diputados</strong> que nos proporcionaran copia de las <strong>facturas de las erogaciones “por concepto de comidas, juntas de trabajo y gastos de operación”</strong> hechas desde septiembre de 2012, cuando inició la LXII Legislatura, hasta ese momento.</span></p>
+				<p><span style="line-height: 1.3em;">La Cámara respondió a la <a href="https://drive.google.com/file/d/0B5VX17x9HKCldHNGdjdqVU9MQ2c/edit?usp=sharing" target="_blank"><strong>solicitud de información 10102</strong></a> que el presupuesto asignado a las comisiones especiales es de $27,000 mensuales, aunque “el monto ejercido por dichos órganos de apoyo legislativo (…) <strong>eroga un promedio mensual de $24,458 por comisión”.</strong></span></p>
+				<p><span style="line-height: 1.3em;">Si partimos de esta última cifra,<strong> estos órganos legislativos costaron 13.3 millones de pesos hasta agosto de 2014</strong>, pero está incompleta. La respuesta de la Unidad de Transparencia sólo da cuenta de los recursos financieros para saldar gastos de operación y manejar un fondo fijo, falta contemplar la asignación de recursos humanos, los vales de alimentos y los servicios de cafetería, edecanes y montaje.</span></p>
+				<p><span style="line-height: 1.3em;">El <a href="https://drive.google.com/file/d/0B5VX17x9HKCleXZUblVxZk1FQm8/edit?usp=sharing" target="_blank"><strong>acuerdo del Comité de Administración</strong></a> sobre los recursos humanos, materiales y financieros de las comisiones especiales firmado el<strong> 26 de septiembre de 2012</strong>, a tres semanas de iniciada la Legislatura, establece un monto de <strong>$108,000 por cada uno de estos órganos</strong>.  Así, la cifra aumenta hasta 59 millones de pesos.</span></p>
+				<p><span style="line-height: 1.3em;">Elaboramos el siguiente cuadro a partir de la última erogación mensual mencionada:</span></p>
+				<iframe width="900" height="200" scrolling="yes" frameborder="no" src="https://www.google.com/fusiontables/embedviz?viz=GVIZ&amp;t=TABLE&amp;q=select+col0%2C+col1%2C+col2%2C+col3%2C+col4+from+16qQWkTW5RksyFmMs8tHZgK-B8IBejOsTHm2Jq1TF&amp;containerId=googft-gviz-canvas"></iframe>
+
+				<p>En otras palabras:</p>
+				<p><span style="font-size: medium;"><strong>-       Cada uno de los 11 informes costó 5.3 millones de pesos.</strong></span></p>
+				<p><span style="font-size: medium;"><strong>-       Cada una de las 139 reuniones costó 425 mil pesos.</strong></span></p>
+				<p><br />Hasta el mes en que presentamos la solicitud de información 10102, en octubre de 2013, <strong>las comisiones especiales habían gastado hasta 23.4 millones de pesos</strong>. Pero sólo tienen la obligación de transparentar los gastos de operación y el fondo fijo, que hasta ese momento, según un cálculo hecho con los datos de la Unidad de Transparencia, <strong>el monto asecendía a 3.3 millones de pesos.</strong></p>
+				<p>Las <strong>facturas obtenidas por transparencia suman 2.3 millones de pesos:</strong> falta un millón de pesos, hasta octubre de 2013.</p>
+				<p>Los 2.3 millones de pesos comprobados se dividen de la siguiente manera. <br /><br <iframe width="900" height="500" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?containerId=googft-gviz-canvas&amp;q=select+col0%2C+col1+from+1171JymD_gBAMc5l_n1bYjp233c-t1nQKWZNX6tn9+order+by+col0+asc+limit+10&amp;viz=GVIZ&amp;t=PIE&amp;uiversion=2&amp;gco_forceIFrame=true&amp;gco_hasLabelsColumn=true&amp;gco_useFirstColumnAsDomain=true&amp;gco_is3D=false&amp;gco_pieHole=0&amp;gco_booleanRole=certainty&amp;gco_colors=%5B%22%233366CC%22%2C%22%23DC3912%22%2C%22%23FF9900%22%2C%22%23109618%22%2C%22%23990099%22%2C%22%230099C6%22%2C%22%23DD4477%22%2C%22%2366AA00%22%2C%22%23B82E2E%22%2C%22%23316395%22%2C%22%23994499%22%2C%22%2322AA99%22%2C%22%23AAAA11%22%2C%22%236633CC%22%2C%22%23E67300%22%2C%22%238B0707%22%2C%22%23651067%22%2C%22%23329262%22%2C%22%235574A6%22%2C%22%233B3EAC%22%2C%22%23B77322%22%2C%22%2316D620%22%2C%22%23B91383%22%2C%22%23F4359E%22%2C%22%239C5935%22%2C%22%23A9C413%22%2C%22%232A778D%22%2C%22%23668D1C%22%2C%22%23BEA413%22%2C%22%230C5922%22%2C%22%23743411%22%5D&amp;gco_hAxis=%7B%22useFormatFromData%22%3Atrue%2C+%22viewWindow%22%3A%7B%22max%22%3Anull%2C+%22min%22%3Anull%7D%2C+%22minValue%22%3Anull%2C+%22maxValue%22%3Anull%7D&amp;gco_vAxes=%5B%7B%22useFormatFromData%22%3Atrue%2C+%22viewWindow%22%3A%7B%22max%22%3Anull%2C+%22min%22%3Anull%7D%2C+%22minValue%22%3Anull%2C+%22maxValue%22%3Anull%7D%2C%7B%22useFormatFromData%22%3Atrue%2C+%22viewWindow%22%3A%7B%22max%22%3Anull%2C+%22min%22%3Anull%7D%2C+%22minValue%22%3Anull%2C+%22maxValue%22%3Anull%7D%5D&amp;width=900&amp;height=500"></iframe><br /><br /></p>
+				<p>Como puede observarse, <strong>el 33% de los gastos corresponde al concepto de alimentos</strong>, a pesar de que mensualmente r<strong>eciben $5,500 en vales de comida.</strong> La proporción cobra sentido al revisar documento fisical por documento. Las visitas a lujosos restaurantes son constantes y el monto de las facturas es sumamente elevado para un país en el que el salario mínimo es de $67 diarios. Inlcluso hay facturas expedidas de madrugada y por cantinas.</p>
+				<p>Por ejemplo, el domingo 10 de marzo de 2013, una comisión especial<strong> facturó $3,216 pesos en el establecimiento La Chilanguita</strong> ubicado en el puerto de Acapulco, Guerrero. El documento fue expedido<strong> a las 02:55 horas.</strong> ¿A esa hora llevarán a cabo trabajos legislativos? Otro ejemplo: el jueves 30 de mayo de 2013 a las 12:48 horas, algún miembro de una comisión especial <strong>pagó $19,491 pesos en el Restaurante Sir Winston Churchill’s de la colonia Polanco</strong>, una de las zonas más exclusivas de la Ciudad de México.</p>
+				<p><iframe src="https://docs.google.com/file/d/0B5VX17x9HKClSWJoNjVsM3BBM0k/view" width="100%" height="480"></iframe></p>
+				<br /><iframe src="https://docs.google.com/file/d/0B5VX17x9HKClS0Znb08xQk5wQUU/view" width="100%" height="480"></iframe>
+				<p>Durante las primeras<strong> 3 semanas de abril de 2014, solicitamos entrevistas con todos los presidentes de las comisiones especiales, pero solo 7 aceptaron.</strong> Hasta ese momento, sólo 8 de quienes se mostraron renuentes a explicar sus gastos y trabajo legislativo había presentado algún informe de actividades.</p>
+				<p>Entrevistamos a:</p>
+				<p>- Gloria Bautista Cuevas, PRD <strong>(Asuntos Alimentarios).</strong></p>
+				<p>- Hugo Jarquín, PRD <strong>(Ciudades Patrimonio de la Humanidad). </strong>          </p>
+				<p>- Jorge Terán Juárez, PRI <strong>(Cuenca del Bajo Pánuco).</strong></p>
+				<p>- Salvador Romero Valencia, PRI <strong>(Industria Automotriz y del Acero).</strong></p>
+				<p>- Mario Miguel Carrillo Huerta, PRD <strong>(Promoción del Desarrollo Regional).</strong></p>
+				<p>- Erick Marte Rivera Villanueva, PAN <strong>(Energías Renovables).</strong></p>
+				<p>- Silvano Blanco De Aquino, PRD <strong>(Impulsar la Agroindustria de la Palma de Coco y Productos Derivados).</strong></p>
+				<p>Los entrevistados aseguraron que su comisión sí trabaja a pesar de que no tienen ni oficina ni presupuesto suficiente para desempeñar sus actividades. “Tenemos un presupuesto tan pequeño que se nos vuelve nada, contamos con 23 mil pesos al mes (…). Nunca estaré de acuerdo que el recurso se desvíe, si se va para cosas que nada tienen que ver con la comisión, condeno esa situación. No es mi caso”, dijo la diputada perredista<strong> Gloria Bautista Cuevas.</strong></p>
+				<p>“A mi comisión le faltan recursos despachamos en un cubículo de 4 por 5 metros, nosotros nos hemos tenido que apretar el cinturón y sí veo mal que quieran justificar gastos con temas que no sean de la comisión”, djo el legislador del PAN <strong>Erick Marte Rivera Villanueva.</strong></p>
+				<p>“Esta comisión siempre esta trabajando si no estamos en reunión aquí en la Cámara, yo o alguno de los integrantes está en reunión con la industria que es tema de nuestra comisión (…). Los gastos de está comisión son totalmente transparentes, todo se va en trabajo y materiales de apoyo para nuestro trabajo, lo que le falta a las comisiones especiales son recursos”, considera el priista<strong> Salvador Romero Valencia.</strong></p>
+				<table style="border-color: #cccccc; border-width: 0px; width: 100%; background-color: #cccccc;" border="0">
+				<tbody>
+				<tr>
+				<td><span style="line-height: 1.3em;">Hasta el 18 de abril de 2014, sólo estas <strong>11 comisiones habían presentado informe de actividades.</strong></span>
+				<p>-Asuntos alimentarios.</p>
+				<p>-De lucha contra la trata de personas.</p>
+				<p>-Para indagar el funcionamiento de las instancias del gobierno federal relacionadas con el otorgamiento de permisos para juegos y sorteos.</p>
+				<p>-Para conocer y dar seguimiento puntual y exhaustivo a las acciones que han emprendido las autoridades competentes con relación a los feminicidios registrados en México.</p>
+				<p>-Desarrollo sustentable.</p>
+				<p>-Fortalecimiento a la educación superior y la capacitación para impulsar el desarrollo y la competitividad.</p>
+				<p>-Cuenca Lerma-Chapala-Santiago.</p>
+				<p>-Cuenca del Bajo Pánuco.</p>
+				<p>-Ciudades patrimonio de la humanidad.</p>
+				<p>-Para dar seguimiento al cumplimiento de los Objetivos de Desarrollo del Milenio.</p>
+				<p>- De Programas Sociales. </p>
+				  </td>
+				</tr>
+				</tbody>
+				</table>
+
+			</div>
+
+		</div>		
+
+	</div>
+
+</div>
---
+++ application/views/notas.php
@@ -0,0 +1,189 @@
+<div class="main">
+
+	<div class="units-row first-main"  >
+	    <div class="unit-100">
+	    
+	        <div class="hiddenmobile" style="display: inline-block;float: left;padding-top: 5px">
+	                    México D.F. a <?=strftime('%A %d de %B de %Y', time())?> 
+	        </div>
+	      
+	        <div style="display: inline-block;padding-top: 1px;">    
+	                <input type="text" name="go" class="input-on-black"  placeholder="Búsqueda" />
+	                <span class="btn-append">
+	                    <button class="btn btn-white btn-outline" style="margin-right:5px;">Go</button>
+	                </span>
+	        </div>
+	    </div>
+	</div>
+
+	<div>
+		
+		<span><h2>Lista de Notas:</h2></span>
+
+	</div>
+
+
+	<div class="units-row">
+		<!-- Columna Izquierda -->
+    	<div class="unit-100">
+    		<span class="bignumber-1">45</span>
+    		<h3>Registros en NOTAS</h3>
+    		<hr>
+
+			<ul class="lista-3">
+
+				<li>
+					<div class="unit-100">
+						
+						<div>
+							<div style="display: inline-block;vertical-align: top;">
+								<img src="images/icons/user.png" width="45" alt="">
+							</div>
+							<div style="display: inline-block;">
+								Redacción  10 Agosto 2014<br/>
+								<a href="">Seguir en Twitter &nbsp;<img src="images/icons/twitter-256.png" width="20" alt=""></a>
+							</div>
+						</div>
+						
+						<a href="">
+						<h3 style="padding:1em;">Diseño e impresión de tríptico y producción de fotografía y video</h3>
+						<div class="units-row">
+							<div class="unit-60">
+								<span style="color:#333;font-size:1.2em;">
+                            		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            	</span>
+							</div>
+							<div class="unit-40">
+								<img class="hiddenmobile" src="http://lorempixel.com/280/120/technics/" alt="">
+								<img class="hiddendesktop" src="http://lorempixel.com/240/280/technics/" alt="">
+							</div>
+							
+						</div>
+						</a>
+					</div>
+					
+				</li>
+
+
+
+
+
+				<li>
+					<div class="unit-100">
+						
+						<div>
+							<div style="display: inline-block;vertical-align: top;">
+								<img src="images/icons/user.png" width="45" alt="">
+							</div>
+							<div style="display: inline-block;">
+								Redacción  10 Agosto 2014<br/>
+								<a href="">Seguir en Twitter &nbsp;<img src="images/icons/twitter-256.png" width="20" alt=""></a>
+							</div>
+						</div>
+						
+						<a href="">
+						<h3 style="padding:1em;">Diseño e impresión de tríptico y producción de fotografía y video</h3>
+						<div class="units-row">
+							<div class="unit-60">
+								<span style="color:#333;font-size:1.2em;">
+                            		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            	</span>
+							</div>
+							<div class="unit-40">
+								<img class="hiddenmobile" src="http://lorempixel.com/280/120/technics/" alt="">
+								<img class="hiddendesktop" src="http://lorempixel.com/240/280/technics/" alt="">
+							</div>
+							
+						</div>
+						</a>
+					</div>
+					
+				</li>
+				<li>
+					<div class="unit-100">
+						
+						<div>
+							<div style="display: inline-block;vertical-align: top;">
+								<img src="images/icons/user.png" width="45" alt="">
+							</div>
+							<div style="display: inline-block;">
+								Redacción  10 Agosto 2014<br/>
+								<a href="">Seguir en Twitter &nbsp;<img src="images/icons/twitter-256.png" width="20" alt=""></a>
+							</div>
+						</div>
+						
+						<a href="">
+						<h3 style="padding:1em;">Diseño e impresión de tríptico y producción de fotografía y video</h3>
+						<div class="units-row">
+							<div class="unit-60">
+								<span style="color:#333;font-size:1.2em;">
+                            		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            	</span>
+							</div>
+							<div class="unit-40">
+								<img class="hiddenmobile" src="http://lorempixel.com/280/120/technics/" alt="">
+								<img class="hiddendesktop" src="http://lorempixel.com/240/280/technics/" alt="">
+							</div>
+							
+						</div>
+						</a>
+					</div>
+					
+				</li>
+				<li>
+					<div class="unit-100">
+						
+						<div>
+							<div style="display: inline-block;vertical-align: top;">
+								<img src="images/icons/user.png" width="45" alt="">
+							</div>
+							<div style="display: inline-block;">
+								Redacción  10 Agosto 2014<br/>
+								<a href="">Seguir en Twitter &nbsp;<img src="images/icons/twitter-256.png" width="20" alt=""></a>
+							</div>
+						</div>
+						
+						<a href="">
+						<h3 style="padding:1em;">Diseño e impresión de tríptico y producción de fotografía y video</h3>
+						<div class="units-row">
+							<div class="unit-60">
+								<span style="color:#333;font-size:1.2em;">
+                            		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            	</span>
+							</div>
+							<div class="unit-40">
+								<img class="hiddenmobile" src="http://lorempixel.com/280/120/technics/" alt="">
+								<img class="hiddendesktop" src="http://lorempixel.com/240/280/technics/" alt="">
+							</div>
+							
+						</div>
+						</a>
+					</div>
+					
+				</li>
+
+    		</ul>
+
+    	</div>
+
+    	
+
+    </div>
+
+	<div class="units-row">
+		<div class="unit-centered unit-20">
+    	
+    		<ul class="pagination">
+			    <li><a href="#">&larr;</a></li>
+			    <li><span>1</span></li>
+			    <li>2</li>
+			    <li>3</li>
+			    <li>4</li>
+			    <li><a href="#">&rarr;</a></li>
+			</ul>
+
+		</div>
+   	</div>
+
+
+</div>
