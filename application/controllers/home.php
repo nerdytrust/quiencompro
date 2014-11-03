0721369128dfce0e826a373c75f79bbd8665a9d2
nerdytrust <contacto@nerdytrust.com>
2014-11-03 12:13:23 -0600

    Type commit message here…

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,12 +2,23 @@
 
 class Home extends CI_Controller {
 
+	function __construct(){
+		parent::__construct();
+		$this->load->model('Quien_model');
+	}
+
 	public function index()
 	{
-		$data = array('titlepage' => '¿ Quién Compró ?' );
+		$header = array('titlepage' => '¿ Quién Compró ?');
+		$body = array(
+			'nota_principal' => $this->Quien_model->get_nota_principal()[0],
+			'notas_recientes'=> $this->Quien_model->get_notas_recientes(),
+			'facturas_recientes'=>$this->Quien_model->get_facturas_recientes(),
+			'banner_der' =>($this->Quien_model->get_banner_der()==null)?null:$this->Quien_model->get_banner_der()[0],
+			);
 
-		$this->load->view('header',$data);
-		$this->load->view('home');
+		$this->load->view('header',$header);
+		$this->load->view('home',$body);
 		$this->load->view('footer');
 	}
 }
